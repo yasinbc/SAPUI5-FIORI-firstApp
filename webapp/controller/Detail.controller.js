75b933435753d9f276d89745a1d034d64bc1f7bf
Yasin Boulahya Chairi <YBC5291@cemosa.es>
2022-04-21 13:43:36 +0200

    Routing Back and History

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History"
+], function (Controller, History) {
 	"use strict";
 	return Controller.extend("CEMOSA.ZYBC_probando.controller.Detail", {
 		onInit: function () {
@@ -12,6 +13,17 @@ sap.ui.define([
 				path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
 				model: "invoice"
 			});
+		},
+		onNavBack: function(){
+			var oHistory = History.getInstance();
+			var sPreviousHash = oHistory.getPreviousHash();
+			
+			if(sPreviousHash !== undefined){
+				window.history.go(-1);
+			}else{
+				var oRouter = this.getOwnerComponent().getRouter();
+				oRouter.navTo("overview", {}, true);
+			}
 		}
 	});
 });
