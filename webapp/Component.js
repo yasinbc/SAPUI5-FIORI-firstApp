abd5d16210c438e18ceaab40475ad5143cf08102
Yasin Boulahya Chairi <YBC5291@cemosa.es>
2022-04-25 10:57:56 +0200

    Device Adaptation

--- webapp/Component.js
+++ webapp/Component.js
@@ -21,6 +21,7 @@ sap.ui.define([
 			};
 			var oModel = new JSONModel(oData);
 			this.setModel(oModel);
+			
 			// disable batch grouping for v2 API of the northwind service
 			//this.getModel("invoice").setUseBatch(false);
 
@@ -35,66 +36,3 @@ sap.ui.define([
 
 	});
 });
-
-
-
-
-
-
-
-// sap.ui.define([
-// 	"sap/ui/core/UIComponent", //Step 9 Walkthrough
-// 	"sap/ui/Device",
-// 	"CEMOSA/ZYBC_probando/model/models",
-// 	"sap/ui/model/json/JSONModel", //Step 9 Walkthrough
-// 	"sap/ui/model/resource/ResourceModel" //Step 9 Walkthrough
-// ], function (UIComponent, Device, models, JSONModel, ResourceModel) {
-// 	"use strict";
-// 	return UIComponent.extend("CEMOSA.ZYBC_probando.Component", {
-
-// 		metadata: {
-// 			//Step 9 Walkthrough
-// 			"interfaces": [sap.ui.core.IAsyncContentCreation],
-// 			"rootView": "CEMOSA.ZYBC_probando.view.App",
-// 			"type": "XML",
-// 			/*"async": true, // implicitly set via the sap.ui.core.IAsyncContentCreation interface*/
-// 			"id": "app",
-
-// 			manifest: "json"
-// 		},
-
-// 		/**
-// 		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-// 		 * @public
-// 		 * @override
-// 		 */
-// 		init: function () {
-// 			// call the base component's init function
-// 			UIComponent.prototype.init.apply(this, arguments); //Step 9 Walkthrough
-// 			//set data model
-// 			var oData = {
-// 				recipient: {
-// 					name: "World"
-// 				}
-// 			};
-
-// 			var oModel = new JSONModel(oData);
-// 			this.setModel(oModel);
-
-// 			//set i18n model
-// 			var i18nModel = new ResourceModel({
-// 				bundleName: "CEMOSA.ZYBC_probando.i18n.i18n"
-// 			});
-// 			this.setModel(i18nModel, "i18n");
-
-// 			// enable routing
-// 			this.getRouter().initialize();
-
-// 			// set the device model
-// 			this.setModel(models.createDeviceModel(), "device");
-			
-// 			//create the views based on the url/hash
-// 			this.getRouter().initialize();
-// 		}
-// 	});
-// });
